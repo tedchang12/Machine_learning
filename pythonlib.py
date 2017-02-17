fed4d94f5f3cbbafda7bccdc69ec09234df0033c
Tedchang102 <a4343102@gmail.com>
2017-02-17 19:24:00 +0800

    implement rest of K-means

--- pythonlib.py
+++ pythonlib.py
@@ -1,15 +1,44 @@
 import matplotlib.pyplot as plt
 import numpy as np
 ##K-means function
+def runkmean(x,center,iterations):
+	
+	for i in range(iterations):
+		
+		index = findClosetcenter(x,center)
+		center = centroidMeans(x,center,index).transpose()
+		plt.plot(center[:,0],center[:,1],'bx',ms=20.0,linewidth=10)
+	kmeanplot(x,index,center)
+	plt.show()
+		
+def kmeanplot(x,index,center):
+	symbol = ['ro','go','bo','rx']
+	for i in range(center.shape[0]):
+		x_class = x[index[:,0]==i]
+		plt.plot(x_class[:,0],x_class[:,1],symbol[i])
+	return 1
+def centroidMeans(x,center,index): ##compute centroid means
+	mean = np.array(())
+	for i in range(center.shape[0]):
+		x_class = x[index[:,0]==i]
+		Ck = len(x_class)
+		if(Ck!=0):
+			Uk = sum(x_class)/Ck
+		else:
+			Uk=np.zeros((1,x.shape[1]))
+		mean = np.append(mean,Uk)
+	return mean.reshape((-1,x.shape[1])).transpose()
+
 def findClosetcenter(x,center): #find the closest center to each node
 	index = []
+	center = center.transpose()
 	for i in range(len(x)):
 		t_center = center.transpose() - x[i,:] #compute x-center(i)
 		diag = np.diag(t_center.dot(t_center.transpose())) #compute ||x-center(i)||^2
-		print(diag)
 		index.append(np.argmin(diag))
 	return np.array(index).reshape((-1,1)) #return minimum index which means the closest to x
 #####need to implement polynomial function
+##logistic Regession
 def logicCost(x,y,theta):
 	hx = sigmoid(x.dot(theta))
 	subsum = (-y*np.log(hx))-((1-y)*np.log(1-hx))
@@ -23,7 +52,8 @@ def logicgrad(x,y,iterations,alpha):
 		subsum = hx-y
 		ini_theta = ini_theta-(((alpha/len(x))* np.dot(np.transpose(x),subsum)))
 	return ini_theta,cost
-
+def sigmoid(n):
+	return np.array(1/(1+np.exp(-1*n)),dtype=np.float128)
 def plotdata(d,d1,s):
 	x = min(d)-1
 	x1 = max(d)+1
@@ -39,6 +69,8 @@ def plotdata(d,d1,s):
 #x = np.arange(-5,5,0.01)
 #y = -(1/(1+np.exp(x)))
 #plotdata(x,y,'r-')
+
+##linear Regression
 def parsedata(filename):
 	data = open(filename,'r')
 	index = 0
@@ -67,6 +99,5 @@ def grad(x,y,alpha,iterations):
 		Theta = Theta-((alpha/len(x)* np.dot(np.transpose(x),subsum)))
 		print(cost)
 	return Theta
-def sigmoid(n):
-	return np.array(1/(1+np.exp(-1*n)),dtype=np.float128)
+
 #print(x[x[:,0]==1])	   get classfy
