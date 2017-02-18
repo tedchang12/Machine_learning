ba7de0c885a72c065b90d8937920d63191ad728b
Tedchang102 <a4343102@gmail.com>
2017-02-19 01:06:43 +0800

    adding Demo function

--- softmax.py
+++ softmax.py
@@ -20,15 +20,13 @@ def main():
 	y = np.array(train_set[1]).reshape(-1,1)
 	theta = np.zeros((x.shape[1],(max(y)+1))) ##random initial theta with feature * #class
 	probM = pylib.genProb(x,max(y)+1,y)  ##[0,0,1,0,0,0,0,0,0,0] if y==2
-	theta = pylib.soft_maxGrad(x,y,theta,0.003,72,probM,xtest,ytest)##start training
+	theta = pylib.soft_maxGrad(x,y,theta,0.003,72,probM,xtest,ytest)##start training, after 70 times iteration the accurance on test set is about 92%
 	###Demo function, random pick up 10 sample
 	DemoRand = np.random.choice(len(ytest),10)
 	for i in range(10):
 		pred = np.argmax(xtest[DemoRand[i],:].dot(theta),axis=0).reshape(-1,1)
 		print(pred)
 		pylib.plot_num(xtest[DemoRand[i],:])
-
-
 main()
 
 
