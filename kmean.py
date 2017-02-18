7a4d39dc4f4a7d92e88642d9cc82bfd7be404586
Tedchang102 <a4343102@gmail.com>
2017-02-19 00:22:03 +0800

    update

--- kmean.py
+++ kmean.py
@@ -7,6 +7,7 @@ import scipy.io as sio
 classfier = 3
 #x = np.array(([1,2,1],[4,5,3]))
 x = sio.loadmat('ML_ex3/ex7data2.mat')['X']
+##print the initial status
 center = np.random.rand(x.shape[1],classfier)+max(x[:,1])/2 #random initial center with #cols * #centers
 center = center.transpose()
 index = pylib.findClosetcenter(x,center)
@@ -14,4 +15,5 @@ mean = pylib.centroidMeans(x,center,index)
 pylib.kmeanplot(x,index,center)
 plt.plot(center[:,0],center[:,1],'bx',ms=20.0,linewidth=10)
 plt.show()
+##start training
 pylib.runkmean(x,center,30)
--- pythonlib.py
+++ pythonlib.py
@@ -1,10 +1,50 @@
 import matplotlib.pyplot as plt
 import numpy as np
+###softmax
+def validSoftmax(xtest,ytest,theta_t,iteration,x,y):
+	pred = np.argmax(xtest.dot(theta_t),axis=1).reshape(-1,1)
+	predx = pred==ytest
+	result = float(sum(predx))/pred.shape[0]
+	predt = np.argmax(x.dot(theta_t),axis=1).reshape(-1,1)
+	predtx = predt==y
+	resultTrain = float(sum(predtx))/predt.shape[0]
+	print("Iteration:%d Precision:%f \nPrecisionTrain:%f" %(iteration,result,resultTrain))
+def plot_num(data):
+	pixel = data.reshape((28,28))
+	plt.imshow(pixel,cmap='gray')
+	plt.show()
+def soft_maxGrad(x,y,theta,lamda,iterations,probM,xtest,ytest):
+	for i in range(iterations):
+		cost,subsum,alpha = softmaxCost(x,y,theta,lamda)
+		prob = -(np.exp(x.dot(theta)-alpha)/subsum)
+		prob = -(x.transpose().dot(prob+probM)/x.shape[0])+lamda*theta
+		theta = theta-prob
+		if(i%10==0):
+			print('Number of iteration:%d Cost:%f'%(i,cost))
+			validSoftmax(xtest,ytest,theta,i,x,y)
+	return theta
+def softmaxCost(x,y,theta,lamda):
+	cost = 0
+	sumXsquare = (sum(sum(x**2)))*lamda/2 ##regularization
+	alpha = np.max(x.dot(theta))
+	subsum = sum(np.nan_to_num(np.exp(x.dot(theta)-alpha)).transpose()).reshape(-1,1)
+	#print(subsum.shape)
+	for i in range(theta.shape[1]):
+		classx = (x[y[:,0]==i])
+		classx = np.exp(classx.dot(theta[:,i])-alpha).reshape(-1,1)
+		classsum = (subsum[y[:,0]==i])
+		cost = cost +sum(classx/classsum)
+	cost = -cost/x.shape[0]+sumXsquare
+	return cost,subsum,alpha
+def genProb(x,t,y):
+	prob = np.zeros((x.shape[0],t))
+	for i in range(len(x)):
+		prob[i,y[i]]=1
+	print(prob.shape)
+	return prob
 ##K-means function
 def runkmean(x,center,iterations):
-	
 	for i in range(iterations):
-		
 		index = findClosetcenter(x,center)
 		center = centroidMeans(x,center,index).transpose()
 		plt.plot(center[:,0],center[:,1],'bx',ms=20.0,linewidth=10)
@@ -25,7 +65,7 @@ def centroidMeans(x,center,index): ##compute centroid means
 		if(Ck!=0):
 			Uk = sum(x_class)/Ck
 		else:
-			Uk=np.zeros((1,x.shape[1]))
+			Uk=np.random.rand((1,x.shape[1]))
 		mean = np.append(mean,Uk)
 	return mean.reshape((-1,x.shape[1])).transpose()
 
