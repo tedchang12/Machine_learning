7e85221c546d0d8d82ae5f824934376f794f0177
Tedchang102 <a4343102@gmail.com>
2017-02-19 00:43:31 +0800

    update

--- pythonlib.py
+++ pythonlib.py
@@ -15,8 +15,8 @@ def plot_num(data):
 	plt.show()
 def soft_maxGrad(x,y,theta,lamda,iterations,probM,xtest,ytest):
 	for i in range(iterations):
-		cost,subsum,alpha = softmaxCost(x,y,theta,lamda)
-		prob = -(np.exp(x.dot(theta)-alpha)/subsum)
+		cost,subsum,alpha,xmultheta = softmaxCost(x,y,theta,lamda)
+		prob = -(np.exp(xmultheta-alpha)/subsum)
 		prob = -(x.transpose().dot(prob+probM)/x.shape[0])+lamda*theta
 		theta = theta-prob
 		if(i%10==0):
@@ -26,16 +26,19 @@ def soft_maxGrad(x,y,theta,lamda,iterations,probM,xtest,ytest):
 def softmaxCost(x,y,theta,lamda):
 	cost = 0
 	sumXsquare = (sum(sum(x**2)))*lamda/2 ##regularization
-	alpha = np.max(x.dot(theta))
-	subsum = sum(np.nan_to_num(np.exp(x.dot(theta)-alpha)).transpose()).reshape(-1,1)
+	xmultheta = x.dot(theta)
+	alpha = np.max(xmultheta)
+	subsum = sum(np.nan_to_num(np.exp(xmultheta-alpha)).transpose()).reshape(-1,1)
 	#print(subsum.shape)
+	"""
 	for i in range(theta.shape[1]):
 		classx = (x[y[:,0]==i])
 		classx = np.exp(classx.dot(theta[:,i])-alpha).reshape(-1,1)
 		classsum = (subsum[y[:,0]==i])
 		cost = cost +sum(classx/classsum)
 	cost = -cost/x.shape[0]+sumXsquare
-	return cost,subsum,alpha
+	"""
+	return cost,subsum,alpha,xmultheta
 def genProb(x,t,y):
 	prob = np.zeros((x.shape[0],t))
 	for i in range(len(x)):
