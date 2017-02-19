bc8cef1a78a3dec54bc2371cc33ba1d0d85dab0b
Tedchang102 <a4343102@gmail.com>
2017-02-19 21:56:48 +0800

    MNIST error rate:30%

--- Kmeans/kmeanonMNIST.py
+++ Kmeans/kmeanonMNIST.py
@@ -3,7 +3,8 @@ import numpy as np
 import scipy.io as sio
 import cPickle,gzip
 from collections import Counter
-def runkmean(x,center,iterations,y,xtest,ytest):
+def runkmean(x,center,iterations,y,xtest,ytest,weighti):
+	maxr = 0
 	for i in range(iterations):
 		index = findClosetcenter(x,center)
 		center = centroidMeans(x,center,index).transpose()
@@ -11,11 +12,40 @@ def runkmean(x,center,iterations,y,xtest,ytest):
 		index_t,resign = resignLabel(index,y)
 		acc = float(sum(index_t==y))/y.shape[0]
 		index_test = findClosetcenter(xtest,center)
+		#print(resign)
+		for j in range(len(index_test)):
+			index_test[j,0] = resign[index_test[j,0],1]
+		
+		for j in range(10):
+			temp = index_test[ytest==j]
+			tempy = ytest[ytest==j] #in 1==1
+			if(temp.shape[0]!=0):
+				accc = float(sum(temp==tempy))/temp.shape[0]
+				#print('num:%d acc:%f' %(j,accc))
+				if(accc<=0.4):
+					tcenter = x[y[:,0]==j]
+					if(tcenter.shape[0]>0):
+						tcenter = tcenter[np.random.choice(tcenter.shape[0],4),:]
+						tcenter = np.sum(tcenter,axis=0)
+						tcenter = tcenter/4
+						center[j,:] = tcenter
+					#print('error')
+		
+		acctest = float(sum(index_test==ytest))/ytest.shape[0]
+		if(maxr<acctest):
+			maxr=acctest
+			print('weight:%d accurance:%f test_accurance:%f' %(i,acc,acctest))
+					
+		
+		
+		"""
 		index_test,resign = resignLabel(index_test,ytest)
 		acctest = float(sum(index_test==ytest))/ytest.shape[0]
-		print('iteration:%d accurance:%f test_accurance:%f' %(i,acc,acctest))
+		"""
+		
 	#kmeanplot(x,index,center)
 	#plt.show()
+	print('weight:%d accurance:%f test_accurance:%f' %(weighti,acc,acctest))
 	return index
 		
 def kmeanplot(x,index,center):
@@ -32,7 +62,9 @@ def centroidMeans(x,center,index): ##compute centroid means
 		if(Ck!=0):
 			Uk = sum(x_class)/Ck
 		else:
-			Uk=np.random.rand(1,center.shape[1])
+			t = x[y[:,0]==i]
+			t = t[np.random.choice(len(t),1),:]
+			Uk=t.reshape(1,-1)
 		mean = np.append(mean,Uk)
 		
 	return mean.reshape((-1,x.shape[1])).transpose()
@@ -57,6 +89,17 @@ def findClosetcenter(x,center): #find the closest center to each node
 		diag = np.diag(t_center.dot(t_center.transpose())) #compute ||x-center(i)||^2
 		index.append(np.argmin(diag))
 	return np.array(index).reshape((-1,1)) #return minimum index which means the closest to x
+def randcenter(x,y):
+	center=np.array(())
+	c = []
+	for i in range(10):
+		temp = x[y[:,0]==i]
+		temp = temp[np.random.choice(len(temp),1),:]
+		c.append(temp)
+		#c.append(x[y[:,0]==i][0,:])
+
+
+	return np.array(c).reshape(-1,10).transpose()
 f = gzip.open('mnist.pkl.gz','rb')
 train_set,valid_set,test_set = cPickle.load(f)
 f.close()
@@ -64,11 +107,17 @@ xtest = np.array(test_set[0])
 ytest = np.array(test_set[1]).reshape(-1,1)
 x = np.array(train_set[0])
 y = np.array(train_set[1]).reshape(-1,1)
-center = np.random.rand(x.shape[1],10)+max(x[:,1])/2
-center = center.transpose()
+center = randcenter(x,y)
+print(center.shape)
+x=x*10
+#center = np.random.choice(len(y),10)
+#center = x[center,:].transpose()
+#center = center.transpose()
 index = findClosetcenter(x,center)
 mean = centroidMeans(x,center,index)
-index = runkmean(x,center,1000,y,xtest,ytest)
+j=2
+
+index = runkmean(x,center,500,y,xtest*10,ytest,5)
 index,resign = resignLabel(index,y)
 acc = float(sum(index==y))/y.shape[0]
 print(acc)
